2332e5d30a6fcaadf4f925f53996b08bdf10e9f7
Leo4077 <129038578+Leo4077@users.noreply.github.com>
2023-08-26 15:49:37 +0800

    0826-2

--- src/20230826.jsx
+++ src/20230826.jsx
@@ -1,15 +1,20 @@
 // props 練習
-function Child({ name }) {
-    return <p>Hello,{name} !</p>
+function Child1({ text }) {
+    return <p>Hello, {text} !</p>
 };
 
+function Child2(props) {
+    return <p>Hello, {props.name} !</p>
+};
 
 function Example() {
     return (
         <div>
-            <Child name="EveryOne" />
+            <Child1 text="EveryOne" />
+            <Child2 name="John" />
         </div>
     );
 }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Example />);
